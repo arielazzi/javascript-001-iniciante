6dd132ecd3b85e56e3cd124b9e30733367200260
Arie_Azzi <ariel_azzi@hotmail.com>
2016-11-10 20:43:40 -0200

    trabalhando com o DOM e iniciando com objetos

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -1,10 +1,19 @@
-var peso = 100;
-var altura = 2.0;
 
-if (altura != 0)
+var tdPeso = document.getElementById("peso-1").textContent;
+var tdAltura = document.getElementById("altura-1").textContent;
+var nome = document.getElementById("nome-1").textContent;
+
+var paciente = {
+				nome   : nome,
+				peso   : tdPeso,
+				altura : tdAltura
+};
+
+if (paciente.altura != 0)
 {
-	var imc = peso / (altura * altura);
-	console.log(imc);
+	var imc = paciente.peso / (paciente.altura * paciente.altura);
+	var tdImc = document.getElementById("imc-1");
+	tdImc.textContent = imc; 
 }
 else
 {
