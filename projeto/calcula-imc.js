0fe45bda260bd6a8fc70a5e515a15b6b4c855fbf
Arie_Azzi <ariel_azzi@hotmail.com>
2016-11-17 20:52:21 -0200

    Eventos

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -1,26 +1,31 @@
-var trsPacientes = document.getElementsByClassName("paciente");
+var botao = document.getElementById("calcula-imcs");
 
-
-
-percorreArray(trsPacientes, function(pacienteTr)
+botao.addEventListener("click", function()
 {
+	var trsPacientes = document.getElementsByClassName("paciente");
 
-	var tdNome = pacienteTr.getElementsByClassName("info-nome")[0].textContent;
-	var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0].textContent;
-	var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0].textContent;
+	percorreArray(trsPacientes, function(pacienteTr)
+	{
 
-	var paciente = { 
-					nome : tdNome, 
-					peso : tdPeso, 
-					altura : tdAltura,
-					pegaImc : function(){
-							if (this.altura != 0)
-								return this.peso / (this.altura * this.altura);
-							else
-							    console.log("Não posso executar uma divisão por 0!");
-						}
-					}; 
-	var imc = paciente.pegaImc();
-	var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
-	tdImc.textContent = imc; 
-});
+		var tdNome = pacienteTr.getElementsByClassName("info-nome")[0].textContent;
+		var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0].textContent;
+		var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0].textContent;
+
+		var paciente = { 
+						nome : tdNome, 
+						peso : tdPeso, 
+						altura : tdAltura,
+						pegaImc : function(){
+								if (this.altura != 0)
+									return this.peso / (this.altura * this.altura);
+								else
+								    console.log("Não posso executar uma divisão por 0!");
+							}
+						}; 
+		var imc = paciente.pegaImc();
+		var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+		tdImc.textContent = imc; 
+	});
+	
+}
+);
